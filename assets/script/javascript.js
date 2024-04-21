4792d0407bb8bcb3e1ff69a1a8d994f5e0ff0932
LiMing Hillman <503055@student.fontys.nl>
2024-04-21 18:51:07 +0200

    added warning alert

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,3 +1,7 @@
+alert(
+  "THIS PAGE IS NOT a real pre-ordering page. Don't enter information you als wouldn't give out to strangers."
+);
+
 scrollDown = () => {
   window.scrollTo(0, document.body.scrollHeight);
 };
