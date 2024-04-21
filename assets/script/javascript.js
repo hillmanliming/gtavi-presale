cc1de1d888cf722242a9c5474dd21e965150886b
LiMing Hillman <503055@student.fontys.nl>
2024-04-21 22:23:31 +0200

    edit warning

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,5 +1,5 @@
 alert(
-  "THIS IS A PHISHING WEBSITE! Don't enter information you als wouldn't give out to strangers."
+  "THIS IS A PHISHING WEBSITE! Don't enter information you als wouldn't give out to strangers. This is a school project."
 );
 
 scrollDown = () => {
