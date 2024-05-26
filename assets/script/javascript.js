f8afebf488060558593d68770b01c75d25755cd8
LiMing Hillman <503055@student.fontys.nl>
2024-05-26 20:12:55 +0200

    removed warning alert

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,7 +1,3 @@
-// alert(
-// 	"THIS IS A PHISHING WEBSITE! Don't enter information you als wouldn't give out to strangers. This is a school project."
-// );
-
 scrollDown = () => {
 	window.scrollTo(0, document.body.scrollHeight);
 };
